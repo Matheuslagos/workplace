f5f4dff2be9d8b8a134f6e7b1957591ece27fd3c
Matheuslagos <matheuslagos@hotmail.com.br>
2023-05-29 23:33:37 -0300

    ajustando templates

--- project/logiq/urls.py
+++ project/logiq/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "logiq"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('documento', views.documento, name='documento'),
+    path('documentoCompleto', views.documentoCompleto, name='documentoCompleto'),
 ]
--- project/logiq/views.py
+++ project/logiq/views.py
@@ -11,3 +11,19 @@ def index(request):
     propriedades = Propriedade.objects.all()
     diagnosticos = Diagnostico.objects.all()
     return render(request, 'logiq/index.html', {'responsaveis': responsaveis, 'produtores': produtores, 'diagnosticos': diagnosticos, 'propriedades': propriedades})
+
+
+def documento(request):
+    responsaveis = Responsavel_Tecnico.objects.all()
+    produtores = ProdutorRural.objects.all()
+    propriedades = Propriedade.objects.all()
+    diagnosticos = Diagnostico.objects.all()
+    return render(request, 'logiq/documento.html', {'responsaveis': responsaveis, 'produtores': produtores, 'diagnosticos': diagnosticos, 'propriedades': propriedades})
+
+
+def documentoCompleto(request):
+    responsaveis = Responsavel_Tecnico.objects.all()
+    produtores = ProdutorRural.objects.all()
+    propriedades = Propriedade.objects.all()
+    diagnosticos = Diagnostico.objects.all()
+    return render(request, 'logiq/documentoCompleto.html', {'responsaveis': responsaveis, 'produtores': produtores, 'diagnosticos': diagnosticos, 'propriedades': propriedades})
